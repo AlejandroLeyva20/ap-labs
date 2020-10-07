c86227b8506734e26dac5ec397f61eaad7547203
Jesus Alejandro Leyva G <a00227182@itesm.mx>
2020-10-06 23:28:49 -0500

    solve file-dir-monitor
    Signed-off-by: Jesus Alejandro Leyva G <a00227182@itesm.mx>

--- labs/file-dir-monitor/logger.c
+++ labs/file-dir-monitor/logger.c
@@ -1,23 +1,93 @@
 #include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <string.h>
+#include <syslog.h>
 #include "logger.h"
+
+int lstdout = 1;
+
+void text(int a, int fg, int bg){
+    char c[20];
+
+	sprintf(c,"%c[%d;%d;%dm", 0x1B, a, fg + 30, bg + 40);
+	printf("%s", c);
+}
 
 int initLogger(char *logType) {
     printf("Initializing Logger on: %s\n", logType);
-    return 0;
+    if(!strlen(logType) || !strcmp(logType, "stdout")) {
+        lstdout = 1;
+    } else if(!strcmp(logType, "syslog")) {
+        lstdout = 0;
+    }
+
+    return lstdout;
 }
 
 int infof(const char *format, ...) {
-    return 0;
+    text(1, 2, 0);
+	va_list a;
+	va_start(a, format);
+    if(lstdout) {
+	    vprintf(format, a);
+    } else {
+        openlog("SYSLOG", 0, 0);
+        vsyslog(LOG_INFO, format, a);
+        closelog();
+    }
+	va_end(a);
+	text(0, 7, 0);
+	return 0;
 }
 
 int warnf(const char *format, ...) {
+    text(1, 3, 0);
+	va_list a;
+	va_start(a, format);
+	if(lstdout) {
+	    vprintf(format, a);
+    } else {
+        openlog("SYSLOG", 0, 0);
+        vsyslog(LOG_WARNING, format, a);
+        closelog();
+    }
+	va_end(a);
+	text(0, 7, 0);
     return 0;
 }
 
 int errorf(const char *format, ...) {
+    text(1, 1, 0);
+	va_list a;
+	va_start(a, format);
+	if(lstdout) {
+	    vprintf(format, a);
+    } else {
+        openlog("SYSLOG", 0, 0);
+        vsyslog(LOG_ERR, format, a);
+        closelog();
+    }
+	va_end(a);
+	text(0, 7, 0);
     return 0;
 }
 int panicf(const char *format, ...) {
+    text(1, 1, 0);
+	va_list a;
+	va_start(a, format);
+
+	if(lstdout) {
+	    vprintf(format, a);
+    } else {
+        openlog("SYSLOG", 0, 0);
+        vsyslog(LOG_EMERG, format, a);
+        closelog();
+    }
+	va_end(a);
+	text(0, 7, 0);
+	raise (SIGABRT);
     return 0;
 }
 
